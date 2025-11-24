473bd9b5dd41dc614fd15394277e2d32aa121257
ronlly <rmapiye@devsdg.digitalgeeksz.pl>
2025-11-24 07:04:56 +0200

    Update Render settings with email configuration

--- mjolobid/settings_render.py
+++ mjolobid/settings_render.py
@@ -167,6 +167,9 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='MjoloBid <mjolobidapp@gmail.com>')
+SERVER_EMAIL = config('SERVER_EMAIL', default='MjoloBid <mjolobidapp@gmail.com>')
+SITE_URL = config('SITE_URL', default='https://your-app-name.onrender.com')
 
 # Stripe configuration
 STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY', default='')
